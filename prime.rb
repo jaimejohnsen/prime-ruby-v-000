d1cec7e079eca32c3c0e3c4c569b063aa752e000
Jaime Johnsen <drivetraindiva@gmail.com>
2018-07-21 17:28:31 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,8 +3,9 @@ def prime?(int)
   if int <=1
     return false
   else
-(2..int).to_a
-int_array = []
+    (2..Math.sqrt(num)).each { |i| return false if num % i == 0}
+#(2..int).to_a
+#int_array = []
  #check for prime divisible by itself and one
 
   end
