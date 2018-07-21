66d8cf16d655001e47050539091039419ccdb581
Jaime Johnsen <drivetraindiva@gmail.com>
2018-07-21 17:34:00 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,16 +4,12 @@ def prime?(int)
     return false
   else
 
-#(2..int).to_a
-#int_array = []
+(2..int).to_a
+int_array = []
  #check for prime divisible by itself and one
 
   end
 end
 
-#def isPrime?(num)
-#        if (2..Math.sqrt(num)).each { |i| return false if num % i == 0}
-#        else
-#          true
 #    end
 #end
