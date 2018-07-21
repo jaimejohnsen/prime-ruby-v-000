e73caeacd44364507cffde1a91f76748ce17ea24
Jaime Johnsen <drivetraindiva@gmail.com>
2018-07-21 18:24:19 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,10 +3,9 @@ def prime?(int)
   if int <=1
     return false
   else
-
-(2..int - 1).to_a.map do |i| int % i == 0
- #check for prime divisible by itself and on
+if (2..int - 1).to_a.map do |i| int % i == 0
     end.include?(true)
+    return false
   end
 end
 
